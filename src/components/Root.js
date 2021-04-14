c73eb20290e192d1e9c04567bfbed5bd4c0d8f07
sterzermatias <matiassterzer@hotmail.com>
2021-04-14 02:21:52 -0300

    arreglo para build

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import List from './List';
 import Detail from './Detail';
-import About from './About';
 
 const Root = () => (
 	<BrowserRouter>
@@ -10,7 +9,6 @@ const Root = () => (
 			<div>
 			<Route exact path="/" component={List}/>
 			<Route exact path="/:id" component={Detail}/>
-			<Route exact path="/about" component={About}/>
 			</div>
 		</Switch>
 	</BrowserRouter>
